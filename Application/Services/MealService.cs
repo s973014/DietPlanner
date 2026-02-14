c9256801f93c171d9ddfd56828101ad8b056f381
Stepan Demchenko <ese4@yandex.ru>
2026-02-14 14:01:34 +0700

    Добавлены сервисы для Meal и Product

---
+++ Application/Services/MealService.cs
@@ -0,0 +1,44 @@
+﻿using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class MealService
+    {
+        private readonly IMealRepository _mealRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MealService(
+            IMealRepository mealRepository,
+            IProductRepository productRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _mealRepository = mealRepository;
+            _productRepository = productRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddProductAsync(
+            Guid mealId,
+            Guid productId,
+            float grams,
+            CancellationToken cancellationToken)
+        {
+            var meal = await _mealRepository.GetByIdAsync(mealId, cancellationToken)
+                ?? throw new InvalidOperationException("Meal not found");
+
+            var product = await _productRepository.GetByIdAsync(productId, cancellationToken)
+                ?? throw new InvalidOperationException("Product not found");
+
+            meal.AddProduct(product, grams);
+
+            _mealRepository.Update(meal);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Application/Services/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(
+            IProductRepository productRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _productRepository = productRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddAsync(Product product, CancellationToken cancellationToken)
+        {
+            await _productRepository.AddAsync(product, cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(Guid productId, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(productId, cancellationToken)
+                ?? throw new InvalidOperationException("Product not found");
+
+            if (await _productRepository.IsUsedInAnyMealAsync(productId, cancellationToken))
+                throw new InvalidOperationException("Cannot delete product used in meals");
+
+            _productRepository.Remove(product);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
