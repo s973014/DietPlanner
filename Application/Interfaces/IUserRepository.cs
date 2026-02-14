a6413c08ecf32375ce01493b0e42ff185654e8fd
Stepan Demchenko <ese4@yandex.ru>
2026-02-14 15:15:04 +0700

    Создание репозитория для пользователя

---
+++ Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<User>> GetAllAsync(CancellationToken cancellationToken = default);
+
+        Task AddAsync(User user, CancellationToken cancellationToken = default);
+        void Update(User user);
+        void Remove(User user);
+    }
+}
---
+++ Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using Application.Interfaces;
+using Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Id == id, cancellationToken);
+        }
+
+        public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+        {
+            return await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Email == email, cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<User>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.Users
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task AddAsync(User user, CancellationToken cancellationToken = default)
+        {
+            await _context.Users.AddAsync(user, cancellationToken);
+        }
+
+        public void Update(User user)
+        {
+            _context.Users.Update(user);
+        }
+
+        public void Remove(User user)
+        {
+            _context.Users.Remove(user);
+        }
+    }
+}
