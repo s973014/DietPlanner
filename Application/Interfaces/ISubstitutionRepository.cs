8bdd031343df443ae53e3f674a69b90dd783295e
Stepan Demchenko <ese4@yandex.ru>
2026-02-15 15:18:25 +0700

    Добавлен репозиторий для замены блюда

---
+++ Application/Interfaces/ISubstitutionRepository.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ISubstitutionRepository
+    {
+        public Task<List<SubstitutionListVm>> GetAllAsync();
+        Task<Substitution?> GetByIdAsync(Guid id);
+        Task AddAsync(Substitution substitution);
+        void Remove(Substitution substitution);
+    }
+
+    public class SubstitutionListVm
+    {
+        public Guid Id { get; set; }
+        public string OriginalMeal { get; set; }
+        public string SubstituteMeal { get; set; }
+        public string Reason { get; set; }
+    }
+}
---
+++ Infrastructure/Data/Repositories/SubstitutionRepository.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interfaces;
+using Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class SubstitutionRepository : ISubstitutionRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SubstitutionRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SubstitutionListVm>> GetAllAsync()
+        {
+            return await _context.Substitutions
+                .Select(s => new SubstitutionListVm
+                {
+                    Id = s.Id,
+                    OriginalMeal = s.OriginalMeal.Name,  
+                    SubstituteMeal = s.SubstituteMeal.Name,
+                    Reason = s.Reason
+                })
+                .ToListAsync();
+        }
+
+        public async Task<Substitution?> GetByIdAsync(Guid id)
+            => await _context.Substitutions
+                .Include(x => x.OriginalMeal)
+                .Include(x => x.SubstituteMeal)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+        public async Task AddAsync(Substitution substitution)
+            => await _context.Substitutions.AddAsync(substitution);
+
+        public void Remove(Substitution substitution)
+            => _context.Substitutions.Remove(substitution);
+    }
+
+}
