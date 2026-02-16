25b9edc337e59ddbda2fafc9f55f47bf8d7bcfa0
Stepan Demchenko <ese4@yandex.ru>
2026-02-16 12:01:04 +0700

    Создание репозитория для недельного плана

---
+++ Application/Interfaces/IWeeklyPlanRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IWeeklyPlanRepository
+    {
+        Task<List<WeeklyPlan>> GetAllAsync();
+        Task<WeeklyPlan?> GetByIdAsync(Guid id);
+        Task AddAsync(WeeklyPlan plan);
+        Task UpdateAsync(WeeklyPlan plan);
+        Task DeleteAsync(Guid id);
+        public IQueryable<WeeklyPlan> GetAllAsyncTracked();
+        public Task<WeeklyPlan?> GetByIdWithMealsAsync(Guid id);
+    }
+}
---
+++ Infrastructure/Data/Repositories/WeeklyPlanRepository.cs
@@ -0,0 +1,68 @@
+﻿using Application.Interfaces;
+using Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class WeeklyPlanRepository : IWeeklyPlanRepository
+    {
+        private readonly AppDbContext _context;
+
+        public WeeklyPlanRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<WeeklyPlan>> GetAllAsync()
+        {
+            return await _context.WeeklyPlans
+                .Include(w => w.DailyMeals)
+                .ThenInclude(dm => dm.Meal)
+                .ToListAsync();
+        }
+
+        public async Task<WeeklyPlan?> GetByIdAsync(Guid id)
+        {
+            return await _context.WeeklyPlans
+                .Include(w => w.DailyMeals)
+                .ThenInclude(dm => dm.Meal)
+                .FirstOrDefaultAsync(w => w.Id == id);
+        }
+
+        public async Task AddAsync(WeeklyPlan plan)
+        {
+            await _context.WeeklyPlans.AddAsync(plan);
+        }
+
+        public async Task UpdateAsync(WeeklyPlan plan)
+        {
+            _context.WeeklyPlans.Update(plan);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var plan = await GetByIdAsync(id);
+            if (plan != null)
+                _context.WeeklyPlans.Remove(plan);
+        }
+
+        public IQueryable<WeeklyPlan> GetAllAsyncTracked()
+        {
+            return _context.WeeklyPlans.AsQueryable();
+        }
+
+        public async Task<WeeklyPlan?> GetByIdWithMealsAsync(Guid id)
+{
+    return await _context.WeeklyPlans
+        .Include(w => w.DailyMeals)
+        .ThenInclude(dm => dm.Meal)
+        .FirstOrDefaultAsync(w => w.Id == id);
+}
+
+    }
+}
